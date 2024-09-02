e745645d67e7e28bd96a7c58c244c0e450ed2c21
Nagata Toshiya <gp23a103@oecu.jp>
2024-09-03 02:10:01 +0900

    [fix]検索機能の修正
    [fix]subsectionの有無で検索結果が表示されない状態を修正

--- script/search.js
+++ script/search.js
@@ -1,13 +1,7 @@
 const sections = window.location.pathname.endsWith('index.html')
-    ? ['index.html', 'sections/section1.html', 'sections/section2.html', 'sections/section3.html', 'sections/section4.html']
-    : ['../sections/section1.html', '../sections/section2.html', '../sections/section3.html', '../sections/section4.html', '../index.html'];
-
-const prefetchedSections = {};
-
-document.addEventListener('DOMContentLoaded', () => {
-    prefetchSections();
-});
-
+    ? ['./index.html', 'sections/section1.html', 'sections/section2.html', 'sections/section3.html', 'sections/section4.html']
+    : [ '../index.html','../sections/section1.html', '../sections/section2.html', '../sections/section3.html', '../sections/section4.html'];
+     
 document.getElementById('searchButton').addEventListener('click', handleSearch);
 document.getElementById('searchInput').addEventListener('keydown', function(event) {
     if (event.key === 'Enter') {
@@ -16,34 +10,21 @@ document.getElementById('searchInput').addEventListener('keydown', function(even
 });
 document.getElementById('navToggle').addEventListener('click', toggleSidebar);
 
-function prefetchSections() {
-    sections.forEach(section => {
-        fetch(section)
-            .then(response => response.text())
-            .then(data => {
-                prefetchedSections[section] = data;
-            })
-            .catch(error => console.error(`セクションの取得エラー: ${section}`, error));
-    });
-}
-
 function handleSearch() {
-    const query = sanitizeQuery(document.getElementById('searchInput').value);
+    const query = document.getElementById('searchInput').value.trim().toLowerCase();
     const resultsContainer = document.getElementById('searchResults');
     resultsContainer.innerHTML = '';
 
     if (query) {
         sections.forEach(section => {
-            const data = prefetchedSections[section];
-            if (data) {
-                processSectionData(data, section, query, resultsContainer);
-            }
+            fetch(section)
+                .then(response => response.text())
+                .then(data => {
+                    processSectionData(data, section, query, resultsContainer);
+                })
+                .catch(error => console.error(`セクションの取得エラー: ${section}`, error));
         });
     }
-}
-
-function sanitizeQuery(query) {
-    return query.trim().toLowerCase().replace(/[^ぁ-んァ-ヶ亜-熙a-zA-Z0-9ー\s]/g, '');
 }
 
 function processSectionData(data, section, query, resultsContainer) {
@@ -84,11 +65,21 @@ function displaySearchResults(mainContent, section, query, textContent, resultsC
     const tags = [...tempDiv.querySelectorAll('[id^="section"], h2, h3')];
 
     searchPositions.forEach(searchPosition => {
-        const { closestSubsectionTag, closestSectionText, closestSubsectionTextContent } = findClosestSectionTag(tags, textContent, searchPosition);
-
-        if (closestSubsectionTag) {
+        const { closestSectionTag, closestSectionText, closestSubsectionTag, closestSubsectionTextContent } = findClosestSectionTag(tags, textContent, searchPosition);
+        console.log(`closestSectionTag:${closestSectionTag}, closestSectionText:${closestSectionText}, closestSubsectionTag:${closestSubsectionTag},closestSubsectionTextContent${closestSubsectionTextContent}`)
+        
+        // h1 -> h2 -> query
+        if (closestSectionTag && closestSubsectionTag) {
             const resultURL = `${section}#${closestSubsectionTag}`;
-            resultsContainer.innerHTML += `<a href="${resultURL}">${closestSectionText}<br>${closestSubsectionTextContent ? closestSubsectionTextContent : ""}</a><br>`;
+            resultsContainer.innerHTML += `<a href="${resultURL}">${closestSectionText}<br>${closestSubsectionTextContent}</a><br>`;
+        }// h1 -> query
+        else if(closestSectionTag && !(closestSubsectionTextContent.includes("補足"))){
+            const resultURL = `${section}#${closestSectionTag}`;
+            resultsContainer.innerHTML += `<a href="${resultURL}">${closestSectionText}<br>${closestSubsectionTextContent}</a><br>`;
+        }// h1 -> query (dosen't exist subsection)
+        else{
+            const resultURL = `${section}#${closestSectionTag}`;
+            resultsContainer.innerHTML += `<a href="${resultURL}">${closestSectionText}</a><br>`;
         }
     });
 }
@@ -105,6 +96,7 @@ function getSearchPositions(textContent, query) {
 
 function findClosestSectionTag(tags, textContent, searchPosition) {
     let closestSectionText = '';
+    let closestSectionTag = '';
     let closestDistance = Infinity;
 
     let closestSubsectionTextContent = '';
@@ -114,17 +106,17 @@ function findClosestSectionTag(tags, textContent, searchPosition) {
     tags.forEach(tag => {
         const tagText = tag.textContent.toLowerCase();
         const tagPosition = textContent.indexOf(tagText);
-
         if (tagPosition !== -1 && tagPosition < searchPosition) {
             const distance = searchPosition - tagPosition;
-
+            
             if (tag.tagName === 'H1' && tag.id.startsWith('section')) {
                 if (distance < closestDistance) {
                     closestDistance = distance;
+                    closestSectionTag = tag.id;
                     closestSectionText = tag.textContent;
                 }
             }
-
+            
             if (tag.tagName === 'H2' || tag.tagName === 'H3') {
                 if (distance < closestSubsectionDistance) {
                     closestSubsectionDistance = distance;
@@ -134,8 +126,7 @@ function findClosestSectionTag(tags, textContent, searchPosition) {
             }
         }
     });
-
-    return { closestSubsectionTag, closestSectionText, closestSubsectionTextContent };
+    return { closestSectionTag, closestSectionText, closestSubsectionTag, closestSubsectionTextContent };
 }
 
 function toggleSidebar() {
